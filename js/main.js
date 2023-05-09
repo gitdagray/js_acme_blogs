e20b980b72acb812effef829d28b7f9bcb58c13d
Trevor Lucas <trevornlucas@gmail.com>
2023-05-08 21:34:06 -0500

    still trying to figure out what broke createPosts

--- js/main.js
+++ js/main.js
@@ -326,10 +326,11 @@ async function displayComments(postId) {
 // input:   posts JSON data
 // output:  returns the fragment element of the posts
 async function createPosts(postsData) {
+    if (!postsData) return;
     // return undefined if postsData is not a JSON object
-    if(typeof postsData !== 'object' ||
-    postsData === null ||
-    Array.isArray(postsData)) return;
+    //if(typeof postsData !== 'object' ||
+    //postsData === null ||
+    //Array.isArray(postsData)) return;
 
     // create fragment
     const fragment = document.createDocumentFragment();
